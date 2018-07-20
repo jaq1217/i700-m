382fb5633a96229859d1b755a102fe129092d002
jaq1217 <jaq1217@163.com>
2018-07-20 15:52:46 +0800

    使用北京成石创新科技有限公司的16in16out IO模块 ECATIO-I16P-O16R 对主站软件进行适配。使用方法和beckhoff的IO模块一样 备份后合并分支

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1252,7 +1252,7 @@ void MainWindow:: UpStatus()
     }
     if(SendPdo && ec_slave[0].state == EC_STATE_OPERATIONAL)
     {
-        //i700-single,EK1100,EL1008,EL2008
+        //i700-single ,ECATDIO-I16-O16
         //i700
         OutputData.TargetPosi = TargetPosi;
         *(ec_slave[1].outputs) = OutputData.ControlWord & 0x00ff;
@@ -1266,9 +1266,10 @@ void MainWindow:: UpStatus()
         *(ec_slave[1].outputs + 20 ) = (OutputData.PosTorLim & 0xff00) >> 8;
         *(ec_slave[1].outputs + 21) = OutputData.NegTorLim & 0x00ff;
         *(ec_slave[1].outputs + 22 ) = (OutputData.NegTorLim & 0xff00) >> 8;
-       //EL2008 output
-        *(ec_slave[4].outputs) = 0x48;
 
+        //ECATDIO-I16-O16
+        *(ec_slave[2].outputs) = 0xaa;
+        *(ec_slave[2].outputs +1) = 0xbb;
 
         ui->infoBrowser->append(str);
         ec_send_processdata();
@@ -1326,8 +1327,10 @@ void MainWindow:: UpStatus()
         ui->TorVallabel->setText(str);//Torque actual value
         str.sprintf("%d",InputData.PosiValue);
         ui->Posilabel->setText(str);//position actual value
-        //EL1008
-        str.sprintf("EL1008 input i:0x%x:",*(ec_slave[3].inputs));
+        //ECATDIO-I16-O16
+        str.sprintf("ECATDIO-I16-O16 input L:0x%x:",*(ec_slave[2].inputs));
+        ui->infoBrowser->append(str);
+        str.sprintf("ECATDIO-I16-O16 input H:0x%x:",*(ec_slave[2].inputs + 1));
         ui->infoBrowser->append(str);
 
     }
